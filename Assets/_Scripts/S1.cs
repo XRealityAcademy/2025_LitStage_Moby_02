b5e006c06af598c513db00e3b932a4f8982708cc
Dominique Wu <info@xrealityacademy.com>
2025-01-26 11:15:33 -0800

    setup done

--- Assets/_Scripts/S1.cs
+++ Assets/_Scripts/S1.cs
@@ -21,6 +21,7 @@ namespace Meta.XR.MRUtilityKit
         public GameObject detectPlaneUI;
         public GameObject[] gameUI;
         public GameObject[] storyObj;
+        private GameObject instantiatedStoryObj;
         private bool isOn;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -37,54 +38,71 @@ namespace Meta.XR.MRUtilityKit
         // Update is called once per frame
         public void PlaceGameObject(Transform t0)
         {
-            Instantiate(storyObj[0], t0.position, t0.rotation);
-            //check if there's any object placed on the table, if yes, then deactive the X mark & storyObj & UI
-            if(GameObject.Find(storyObj[0].name)!=null)
-            {    
-                placeMesh.SpawnOnFlatSurface(!isOn);            
-                gameUI[0].SetActive (false);
+            if (instantiatedStoryObj == null) // Ensure no duplicate instantiations
+            {
+                instantiatedStoryObj = Instantiate(storyObj[0], t0.position, t0.rotation);
+                Debug.Log($"Placed {instantiatedStoryObj.name} at {t0.position}");
+                // Check if the object exists, then deactivate relevant UI
+                if (instantiatedStoryObj != null)
+                {
+                    placeMesh.SpawnOnFlatSurface(!isOn);
+                    gameUI[0].SetActive(false);
+                }
             }
-       
+            else
+            {
+                Debug.LogWarning("An object is already placed. Remove it before placing another.");
+            }
         }
 
         public void YesConfirm()
         {
+            
             ////place the cube on the surface
            // PlaceGameObject(placeMesh._debugCube.transform); 
             confirmedUI.SetActive(false);
 
         }
 
+        // Handle the "No" confirmation logic
         public void NoConfirm()
         {
-            Destroy(storyObj[0]);
+            if (instantiatedStoryObj != null)
+            {
+                Destroy(instantiatedStoryObj);
+                instantiatedStoryObj = null;
+                Debug.Log("Destroyed the placed story object.");
+            }
+            else
+            {
+                Debug.LogWarning("No object to destroy.");
+            }
             confirmedUI.SetActive(false);
             detectPlaneUI.SetActive(true);
-            gameUI[0].SetActive (true); 
-            placeMesh.SpawnOnFlatSurface(isOn);   
-
-
-        }  
-        public void RemoveGameObject(Transform t0)
-        {      
-            //check if there's any object placed on the table, if yes, then deactive the X mark & storyObj & UI
-            if(GameObject.Find(storyObj[0].name)!=null)
-            {   
-                //destroy the current house/story object
-                Destroy(storyObj[0]);
-                //detect UI shows up
-                detectPlaneUI.SetActive(true);
-                gameUI[0].SetActive (true);  
-                //Keep looking for the new spot 
-                placeMesh.SpawnOnFlatSurface(isOn);         
-
-            }    
+            gameUI[0].SetActive(true);
+            placeMesh.SpawnOnFlatSurface(isOn);
         }
+        // Remove the placed object and reset the UI
+        public void RemoveGameObject(Transform t0)
+        {
+            if (instantiatedStoryObj != null)
+            {
+                Destroy(instantiatedStoryObj);
+                instantiatedStoryObj = null;
+                Debug.Log("Removed the placed story object.");
+                detectPlaneUI.SetActive(true);
+                gameUI[0].SetActive(true);
+                placeMesh.SpawnOnFlatSurface(isOn);
+            }
+            else
+            {
+                Debug.LogWarning("No object found to remove.");
+            }
+        }
+        
 
 
     }
-
-
 
 
 
