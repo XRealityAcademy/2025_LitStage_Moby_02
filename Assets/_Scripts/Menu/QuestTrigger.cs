fd71cac09d8bec1911dda81530711ac4d28afb70
Dominique Wu <info@xrealityacademy.com>
2025-03-08 22:52:19 -0800

    quest done!

--- Assets/_Scripts/Menu/QuestTrigger.cs
+++ Assets/_Scripts/Menu/QuestTrigger.cs
@@ -4,7 +4,8 @@ using Meta.XR.MRUtilityKit;
 public class QuestTrigger : MonoBehaviour
 {
     public QuestUiToggle questUiToggle; // Reference to the QuestManager script
-    private int currentQuestIndex = 0; // Tracks the current quest to toggle
+    private int currentQuestIndex = -1; // Tracks the current quest to toggle
+
     bool aButtonPressed = false;
     void Start()
     {
@@ -14,19 +15,21 @@ public class QuestTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(OVRInput.GetDown(OVRInput.Button.Two)) 
+       if(OVRInput.GetDown(OVRInput.RawButton.B)) 
        {    
-            ToggleNextQuest();
+            IncrementQuestIndex();
        }
         
     }
-    void ToggleNextQuest()
+    void IncrementQuestIndex()
     {
-
-        if (currentQuestIndex >= questUiToggle.quests.Length) return; // Stop if all quests are toggled
+        currentQuestIndex++;
+        if (currentQuestIndex >= questUiToggle.quests.Length)
+        {
+            currentQuestIndex = questUiToggle.quests.Length; // Cap index at quests length
+            return;
+        }
 
         questUiToggle.ToggleQuestCompletion(currentQuestIndex); // Toggle current quest completion
-        currentQuestIndex++; // Move to the next quest
     }
-
 }
